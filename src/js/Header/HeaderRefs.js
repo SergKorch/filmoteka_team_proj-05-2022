8a141fd6344c057306beb7a939f457fd816eb8ef
SergKorch <90097476+SergKorch@users.noreply.github.com>
2022-05-22 15:02:37 +0000

    Deploying to gh-pages from @ SergKorch/filmoteka_team_proj2022@2cbb7824821a46952acce3d69cfdbb540d3dc0c8 🚀

--- src/js/Header/HeaderRefs.js
+++
@@ -1,8 +0,0 @@
-export const REF = {
-  LOGO: document.querySelector('#site-logo'),
-  HOME: document.querySelector('#home'),
-  LIBRARY: document.querySelector('#library'),
-  CONTAINER: document.querySelector('.page-container'),
-};
-
-export const MOVIE_GALLERY_REF = document.querySelector('#movie-gallery');
--- src/js/spinner.js
+++
@@ -1,8 +0,0 @@
-export function onLoadSpinner () {
-      const spinner = document.querySelector(".spinner-wrapper");
-        spinner.classList.remove('visually-hidden');
-    };
-export function offLoadSpinner () {
-    const spinner = document.querySelector(".spinner-wrapper");
-      spinner.classList.add('visually-hidden');
-  };
